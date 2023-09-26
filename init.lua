57c3dd4e9a1b0507df7e157ecb46bd814190c473
Ryan Shi <ryan.shi42@gmail.com>
2023-09-26 19:37:24 +1000

    added git integration and comments

--- init.lua
+++ init.lua
@@ -4,20 +4,43 @@ require("config.lazy")
 local call = vim.call
 local cmd = vim.cmd
 local Plug = vim.fn["plug#"]
-local PATH = ""
+local PATH = "~/.config/nvim/plugged"
 
 call("plug#begin", PATH)
+
+-- Dracula NVim
 Plug("Mofiqul/dracula.nvim")
+
+-- Vim Easy Align
 Plug("junegunn/vim-easy-align")
-Plug("preservim/nerdtree")
+
+-- GitHub Dashboard
 Plug("https://github.com/junegunn/vim-github-dashboard.git")
 
+-- Autocomplete Code from small Snippets
 Plug("SirVer/ultisnips")
 Plug("honza/vim-snippets")
+
+--
+
+-- Intellisense
 Plug("neoclide/coc.nvim", { branch = "release" })
+
+-- Directory Tree
 Plug("scrooloose/nerdtree", { on = "NERDTreeToggle" })
 
+-- Status Bar
+Plug("vim-airline/vim-airline")
+
+-- Fuzzy Finder
+Plug("nvim-lua/plenary.nvim")
+Plug("nvim-telescope/telescope.nvim", { tag = "0.1.3" })
+
+-- Git Integration
+Plug("tpope/vim-fugitive")
+
 call("plug#end")
+
 -- vim.cmd[[colorscheme dracula]]
 --
 --
